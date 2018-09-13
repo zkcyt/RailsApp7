5181b73f1b216b71b892ac62ce58a6e04620c1d3
zkcyt <tmsinksz@gmail.com>
2018-09-13 09:24:30 +0900

    いいね機能途中3　(いいね数・ボタン認証 色変化に問題)

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,51 +1,20 @@
 class LikesController < ApplicationController
   #layout 'likes'
-  before_action :set_post
-  require 'net/http' # この一行を追加する
+  # before_action :logged_in_user
+  #before_action :authenticate_account
+  before_action :set_post, only: [:create, :destroy]
 
   def create
-    #@like = Like.new(user_id: current_account.id, post_id: params[:post_id])
-    #@like.save
-    # @post = Post.find(params[:post_id])
-    # @post.like(current_user)
      @like = Like.create(user_id: current_account.id, post_id: params[:post_id])
-     #@likes = Like.where(post_id: params[:post_id])
-     #@posts = Post.all
+  end
 
-     # respond_to do |format|
-        # format.html
-        # format.js
-   end
-
-   def destroy
+  def destroy
     like = Like.find_by(user_id: current_account.id, post_id: params[:post_id])
     like.destroy
-  # @post = Like.find(params[:id]).post
-   # @post.unlike(current_user)
-    #@likes = Like.where(post_id: params[:post_id])
-    #@posts = Post.all
+  end
 
-     # respond_to do |format|
-      #  format.html
-      #  format.js
-   end
-
-   private
-
+  private
     def set_post
       @post = Post.find(params[:post_id])
     end
-
- # def create
-  #  @like = Like.new(user_id: @current_user.id, post_id: params[:post_id])
-  #  @like.save
-  #  redirect_to("/posts/#{params[:post_id]}")
-  # end
-
-  # def destroy
-    # @like = Like.find_by(user_id: @current_user.id, post_id: params[:post_id])
-    # @like.destroy
-    # redirect_to("/posts/#{params[:post_id]}")
-  # end
-
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
-  #has_many :like_users, through: :likes, source: :user
 
- # ポストをいいねする
- def like_user(user_id)
-   likes.find_by(user_id: user_id)
- end
+   def like_user(user_id)
+     likes.find_by(user_id: user_id)
+   end
 
+   validates :title, :content, presence: {message: 'は必須項目です。'}
+  end
 
  #def like_user(user_id)
    #likes.find_by(user_id: user_id)
@@ -21,6 +21,3 @@ class Post < ApplicationRecord
   # def unlike(user)
     # likes.find_by(user_id: user.id).destroy
   # end
-
-  validates :title, :content, presence: {message: 'は必須項目です。'}
-end
